9c4e41d07f616ced593ed98a1a89a1de0f4d9863
MihirVir <128705226+MihirWii@users.noreply.github.com>
2023-04-01 22:41:49 +0530

    client depl yaml added

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,31 +1,35 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 const PostCreate = () => {
-    const [title, setTitle] = useState('');
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const [title, setTitle] = useState("");
 
-        await axios.post('http://localhost:4000/posts', {
-            title: title
-        });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        setTitle('');
-    }
-    return (
+    await axios.post("http://posts.com/posts", {
+      title: title,
+    });
+
+    setTitle("");
+  };
+  return (
     <>
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label>Title</label>
-                    <input value = {title} onChange ={e => setTitle(e.target.value)} className = "form-control mb-2 mt-2"/>
-                </div>
-                <button className = "btn btn-primary">Submit</button>
-            </form>
-        </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Title</label>
+            <input
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="form-control mb-2 mt-2"
+            />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default PostCreate
+export default PostCreate;
